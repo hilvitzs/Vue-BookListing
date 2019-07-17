fda25226a35f347e4518a58245dd13e910d69ca5
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2019-07-17 15:36:28 -0700

    Through module 5 (6)

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,38 +2,47 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <book-item v-for="book in books" :book=book></book-item>
+      <book-item v-for="book in books" :book="book"></book-item>
     </ul>
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 <script>
-  import BookItem from './BookItem';
+import BookItem from "./BookItem";
+import BookForm from "./BookForm";
 
-  export default {
-    name: 'BookList',
-    components: {
-      BookItem,
-    },
-    data() {
-      return {
-        title: 'All Books',
-        books: [
-          { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
-          { title: 'American Gods', author: 'Neil Gaiman' },
-          { title: 'Amusing ourselves to Death', author: 'Neil Portman' }
-        ]
-      }
+export default {
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing ourselves to Death", author: "Neil Portman" }
+      ]
+    };
+  },
+  components: {
+    BookItem,
+    BookForm
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor })
     }
   }
+};
 </script>
 <style>
-  h1, h2 {
-    font-weight: normal;
-  }
+h1,
+h2 {
+  font-weight: normal;
+}
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 </style>
 
