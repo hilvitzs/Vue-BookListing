ba2118601fb2a73dff9d928b2fad41e47979d120
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2019-07-17 15:08:58 -0700

    Through module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books">{{ book.title}}: {{ book.author }}</li>
+    </ul>
   </div>
 </template>
 <script>
@@ -8,7 +11,12 @@
     name: 'BookList',
     data() {
       return {
-        title: 'All Books'
+        title: 'All Books',
+        books: [
+          { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+          { title: 'American Gods', author: 'Neil Gaiman' },
+          { title: 'Amusing ourselves to Death', author: 'Neil Portman' }
+        ]
       }
     }
   }
@@ -16,6 +24,16 @@
 <style>
   h1, h2 {
     font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: block;
+    margin: 0 10px;
   }
 </style>
 
